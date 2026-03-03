cac043ab89655f9290ff38097c48c3642d52aa2b
Gustavo Mendes <gustavomlima.contato@gmail.com>
2026-03-03 04:57:01 -0300

    improve UI, add input validation

--- Weather-API.Frontend/script.js
+++ Weather-API.Frontend/script.js
@@ -1,32 +1,45 @@
-const button = document.getElementById('searchBtn');
-const resultDiv = document.getElementById('result');
+document.addEventListener('DOMContentLoaded', () => {
 
-button.addEventListener('click', async () => {
-  const city = document.getElementById('cityInput').value;
+  const button = document.getElementById('searchBtn');
+  const resultDiv = document.getElementById('result');
+  const input = document.getElementById('cityInput');
 
   const cityRegex = /^[A-Za-zÀ-ÿ\s-]+$/;
 
-    if (!cityRegex.test(city)) {
-    resultDiv.innerHTML = 'Cidade inválida';
-    return;
+  async function getWeather(city) {
+
+    if (!city) {
+      resultDiv.innerHTML = 'Digite uma cidade';
+      return;
     }
 
-  if (!city) {
-    resultDiv.innerHTML = 'Digite uma cidade';
-    return;
+    if (!cityRegex.test(city)) {
+      resultDiv.innerHTML = 'Cidade inválida';
+      return;
+    }
+
+    try {
+      resultDiv.classList.remove('show');
+      resultDiv.innerHTML = `<div class="loading">Buscando clima...</div>`;
+      const response = await fetch(`http://localhost:5000/api/weather/${city}`);
+      const data = await response.json();
+
+      resultDiv.innerHTML = `
+        <h2>${data.city}</h2>
+        <div class="temp">${data.temperature}°C</div>
+        <div class="desc">${data.description}</div>
+      `;
+      resultDiv.classList.add('show');
+
+    } catch (error) {
+      resultDiv.innerHTML = 'Erro ao buscar clima';
+    }
   }
 
-  try {
-    const response = await fetch(`http://localhost:5000/api/weather/${city}`);
-    const data = await response.json();
+  button.addEventListener('click', () => {
+    getWeather(input.value.trim());
+  });
 
-    resultDiv.innerHTML = `
-      <h2>${data.city}</h2>
-      <p>Temperatura: ${data.temperature}°C</p>
-      <p>Condição: ${data.description}</p>
-    `;
+  getWeather('Praia Grande');
 
-  } catch (error) {
-    resultDiv.innerHTML = 'Erro ao buscar clima';
-  }
 });
